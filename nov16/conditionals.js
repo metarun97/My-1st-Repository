c60178d5c9ca34b74f76b0d7df8d1a9ce75b99e0
metarun97 <116960857+metarun97@users.noreply.github.com>
2022-11-16 19:09:57 +0530

    Add files via upload

---
+++ nov16/conditionals.js
@@ -0,0 +1,44 @@
+let hour = 10
+
+if (hour < 12) {
+console.log('good morning')
+}
+
+
+let hour = 13
+
+if (hour < 12) {
+console.log('good morning')
+} 
+
+
+let hour = 13
+
+if (hour < 12) {
+console.log('good morning')
+}else {
+console.log('good afternoon')
+}
+
+
+let hour = 12
+
+if (hour < 12) {
+console.log('good morning')
+}else if (hour<20){
+console.log('good day')
+}else {
+console.log('good evening')
+
+} 
+
+let hour = 22
+
+if (hour < 12) {
+console.log('good morning')
+}else if (hour<20){
+console.log('good day')
+}else {
+console.log('good evening')
+
+} 
