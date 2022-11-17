c2dc1d8efbed96dd0f5bd683cd6c5698443f7fc7
metarun97 <116960857+metarun97@users.noreply.github.com>
2022-11-17 20:46:54 +0530

    Update conditionals.js

--- nov16/conditionals.js
+++ nov16/conditionals.js
@@ -34,6 +34,150 @@ console.log('good evening')
 
 let hour = 22
 
+
+let currentyear = 2022
+
+if (currentyear == 2022){
+console.log('you are so smart')
+}
+
+
+let currentyear = 2021
+
+if (currentyear == 2022){
+console.log('you are so smart')
+}
+
+
+let currentyear = 2023
+
+if (currentyear == 2022){
+console.log('you are so smart')
+}else{
+console.log('you are wrong')
+}
+
+let currentyear = 2023
+
+If (currentyear == 2022){
+console.log('you are so smart')
+}else{
+console.log('you are wrong')
+}
+
+let currentyear = 2023
+
+IF (currentyear == 2022){
+console.log('you are so smart')
+}else{
+console.log('you are wrong')
+}
+
+
+let accessAllowed
+
+let age = 16
+if(age > 18){
+accessAllowed = true
+}else{
+accessAllowed = false
+}
+
+console.log(accessAllowed)
+
+
+let accessAllowed
+
+let age = 19
+if(age > 18){
+accessAllowed = true
+}else{
+accessAllowed = false
+}
+
+console.log(accessAllowed)
+
+
+
+let age = 19
+let accessAllowed = (age > 18) ? true : false
+
+console.log(accessAllowed)
+
+
+let age = 10
+
+let message = (age <3) ? 'hey small kid':
+(age <18) ? 'hey kid' : (age <100) ? 'greetings' :
+'legends'
+
+console.log(message)
+
+
+let age = 2
+
+let message = (age <3) ? 'hey small kid':
+(age <18) ? 'hey kid' : (age <100) ? 'greetings' :
+'legends'
+
+console.log(message)
+
+
+let age = 200
+
+let message = (age <3) ? 'hey small kid':
+(age <18) ? 'hey kid' : (age <100) ? 'greetings' :
+'legends'
+
+console.log(message)
+
+
+
+let age = 200
+
+let message
+
+if (age <3){
+message = 'hey small kid'
+} else if(age <100){
+message = 'greetings'
+} else {
+message = 'legends'
+}
+
+console.log(message)
+
+
+let age = 2
+
+let message
+
+if (age <3){
+message = 'hey small kid'
+} else if(age <100){
+message = 'greetings'
+} else {
+message = 'legends'
+}
+
+console.log(message)
+
+
+let age = 80
+
+let message
+
+if (age <3){
+message = 'hey small kid'
+} else if(age <100){
+message = 'greetings'
+} else {
+message = 'legends'
+}
+
+console.log(message)
+
+
 if (hour < 12) {
 console.log('good morning')
 }else if (hour<20){
