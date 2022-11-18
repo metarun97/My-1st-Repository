e56c4f48dfeab59e0bd1958a28408106286062f4
TARUN UPADHYAY <116960857+metarun97@users.noreply.github.com>
2022-11-18 13:00:53 +0530

    Add files via upload

---
+++ nov18/switch.js
@@ -0,0 +1,104 @@
+let browser = 'chrome'
+
+switch(browser) {
+case 'edge':
+console.log('you have selected edge browser')
+
+case 'chrome' :
+console.log('you have selected chrome browser')
+
+case 'opera' :
+console.log('you have selected opera browses')
+
+default:
+console.log('we do not support this browser please select again')
+}
+
+
+let browser = 'chrome'
+
+switch(browser) {
+case 'edge':
+console.log('you have selected edge browser')
+
+break;
+
+case 'chrome' :
+console.log('you have selected chrome browser')
+
+break;
+
+case 'opera' :
+console.log('you have selected opera browses')
+
+break;
+default:
+console.log('we do not support this browser please select again')
+}
+
+
+let browser = 'safari'
+
+switch(browser) {
+case 'edge':
+console.log('you have selected edge browser')
+
+break;
+
+case 'chrome' :
+console.log('you have selected chrome browser')
+
+break;
+
+case 'opera' :
+console.log('you have selected opera browses')
+
+break;
+default:
+console.log('we do not support this browser please select again')
+}
+
+
+let arrowselected = 'up'
+switch(arrowselected) {
+
+case 'up':
+console.log('move up')
+break;
+case 'down' :
+console.log('move down')
+break;
+case 'left' :
+console.log('move left')
+break;
+default:
+console.log('right')
+}
+
+let browser = 'edge'
+
+switch(browser) {
+case 'edge':
+case 'chrome':
+case 'opera' :
+console.log('we support this browser')
+
+break;
+default:
+console.log('we do not support this browser please select again')
+}
+
+
+
+let browser = 'safari'
+
+switch(browser) {
+case 'edge':
+case 'chrome':
+case 'opera' :
+console.log('we support this browser')
+
+break;
+default:
+console.log('we do not support this browser please select again')
+}
