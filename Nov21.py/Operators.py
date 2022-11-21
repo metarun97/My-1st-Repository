51baa6ba3700d07fb13f21e3914f66bfa23846ff
TARUN UPADHYAY <116960857+metarun97@users.noreply.github.com>
2022-11-21 20:33:27 +0530

    Add files via upload

---
+++ Nov21.py/Operators.py
@@ -0,0 +1,92 @@
+#                                                 [Arithmatic Operators]
+
+# 10 20 'hi' True
+# In (10 + 20) So, 
+# +  is the operator.
+10 > 20
+# 10 in (10,20) in it (in) operator.
+
+
+# 1. Arithmatic Operators
+
+# + - * / ** // % 
+
+
+#(INPUTS)             #(OUTPUTS)
+
+
+num1 = 100
+num2 = 50
+print(num1 + num2)    # 150
+print(num1 - num2)    # 50
+print(num1 * num2)    # 5000
+print(num1 / num2)    # 2.0 
+print(num1 % num2)    # 0
+
+
+print(3 % 2)          # 1
+print( 10 / 3)        # 3.3333333333333335
+print(10 // 3)        # 3
+print(10 // 7)        # 1
+print(585858 / 5)     # 117171.6
+print(585858 // 5)    # 117171
+
+
+res = 10 // 7
+print(res,type(res))  # 1 <class 'int'>
+
+
+print(8 ** 2)         # 64
+#It is exponential condtion.
+
+
+num = 100
+print(num // 25)      # 4
+
+num = num // 25       # 4
+print(num)
+
+
+a = 10
+a+= 1
+print(a)              # 11
+
+
+a = 10
+a-= 1
+print(a)              # 9
+
+
+# i++= i = i + 1 = i + 1
+
+
+
+#                                      [Assignment Operators]
+
+
+
+
+#Assignment Operators (=)
+#Assigned a value (literal) to a variable
+
+
+#(Input)                        #(Output)
+
+joy = 100
+pilot = True
+
+print(joy,type(joy))          # 100<class'int'>
+print(pilot,type(pilot))      # True<class'bool'>
+
+false = 'Hi'                  #SyntaxError: cannot assign to False
+
+
+10000000 = house              #SyntaxError: cannot assign to literal 
+
+
+
+
+
+
+
+
