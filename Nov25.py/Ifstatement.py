c672a7ccf8050570f9cbc74bf9967b1ccb8bdf2c
TARUN UPADHYAY <116960857+metarun97@users.noreply.github.com>
2022-11-25 20:46:04 +0530

    Add files via upload

---
+++ Nov25.py/Ifstatement.py
@@ -0,0 +1,47 @@
+# Conditional statements and loops
+# if else,for,while,break,continue,pass.
+#IF ELSE BLOCK:-
+#if(expressions):
+#  (statements...)
+#  (sta.....)
+  
+  
+#Area of a figure
+
+#sides = int(input("Enter number of sides : "))
+
+#Area of circle:-
+
+
+#             (INPUT)                                                         (OUTPUT)
+
+sides = int(input("Enter number of sides : "))
+if (sides == 1):                                                       # Enter number of sides : 1
+    rad = int(input("Enter the radius : "))                            # Enter the radius : 3
+    print('Area of circle = ', 3.14 * rad * rad)                       # Area of circle =  28.259999999999998
+
+
+sides = int(input("Enter number of sides : "))
+if (sides == 3) :                                                      # Enter number of sides : 3
+    p = int(input("Enter Perpenicular : "))                            # Enter Perpenicular : 10
+    b = int(input("Enter base : "))                                    # Enter base : 25
+    print('Area of Triangle = ', 0.5 * p * b)                          # Area of Triangle =  125.0
+
+
+#Area of reactangle
+
+
+sides = int(input("Enter number of sides : "))
+if(sides == 4) :                                                       # Enter number of sides : 4
+   l = int(input("Enter length : "))                                   # Enter length : 10
+   b = int(input("Enter base : "))                                     # Enter base : 25
+   print('Area of Reactangle = ', l * b)                               # Area of Reactangle =  250
+
+
+
+
+
+
+
+
+
