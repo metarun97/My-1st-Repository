e8cc4a717182d9c6181f153d1018387c536ad628
TARUN UPADHYAY <116960857+metarun97@users.noreply.github.com>
2022-11-26 17:59:19 +0530

    Update Ifstatement.py

--- Nov25.py/Ifstatement.py
+++ Nov25.py/Ifstatement.py
@@ -39,6 +39,119 @@ if(sides == 4) :                                                       # Enter n
 
 
 
+#IF ELSE BLOCK:-
+
+# if (expression) :
+#     hkbjdciibkcg
+# else
+#     jbjbfsvvbab
+
+# Area of circle:-
+
+
+sides = int(input("Enter number of sides : "))
+if (sides == 1):                                                        # Enter number of sides : 1
+    rad = int(input("Enter the radius : "))                             # Enter the radius : 10
+    print('Area of circle = ', 3.14 * rad * rad)                        # Area of circle =  314.0
+else:
+    print("TRY AGAIN")
+
+# Input we out 2 in the (Enter number of sides : ) then it shows (TRY AGAIN).
+# Because it is false output given in that it shows (TRY AGAIN) .
+
+
+# IF ELIF ELSE BLOCK:-
+
+# if (expression):
+#    bjkbsacvdv
+#    bjbcksbvsv
+# elif(expression):
+#    bjsbcacvja
+# else:
+#    nkiavbabb
+
+
+
+# Area of figure:-
+sides = int(input("Enter number of sides : "))
+if (sides ==1):
+    rad = int(input("Enter the radius : "))
+    print('Area of circle = ', 3.14 * rad * rad) 
+    
+#Area of Triangle:-
+elif(sides == 3):
+    p = int(input("Enter Perpendicular : "))
+    b = int(input("Enter Base : "))
+    print('Area of triangle = ', 0.5 *p * b)
+    
+#Area of rectangle:-
+elif(sides == 4):
+    l = int(input("Enter Length : "))
+    b = input("Enter Base : ")
+    print('Area of Rectangle = ', l * b)
+else:
+    print("TRY AGAIN")
+
+#     (OUTPUTS)
+# Enter number of sides : 1
+# Enter the radius : 3
+# Area of circle =  28.259999999999998
+  
+
+# Area of figure:-
+sides = int(input("Enter number of sides : "))
+if (sides ==1):
+    rad = int(input("Enter the radius : "))
+    print('Area of circle = ', 3.14 * rad * rad) 
+    
+#Area of Triangle:-
+elif(sides == 3):
+    p = int(input("Enter Perpendicular : "))
+    b = int(input("Enter Base : "))
+    print('Area of triangle = ', 0.5 *p * b)
+    
+#Area of rectangle:-
+elif(sides == 4):
+    l = int(input("Enter Length : "))
+    b = input("Enter Base : ")
+    print('Area of Rectangle = ', l * b)
+else:
+    print("TRY AGAIN")
+
+
+# Enter number of sides : 3
+# Enter Perpendicular : 5
+# Enter Base : 7
+# Area of triangle =  17.5
+
+
+# Area of figure:-
+sides = int(input("Enter number of sides : "))
+if (sides ==1):
+    rad = int(input("Enter the radius : "))
+    print('Area of circle = ', 3.14 * rad * rad) 
+    
+    #Area of Triangle:-
+elif(sides == 3):
+    p = int(input("Enter Perpendicular : "))
+    b = int(input("Enter Base : "))
+    print('Area of triangle = ', 0.5 *p * b)
+    #Area of rectangle:-
+elif(sides == 4):
+    
+    l = int(input("Enter Length : "))
+    b = int(input("Enter Base : "))
+    print('Area of Rectangle = ', l * b)
+else:
+    print("TRY AGAIN")
+
+# Enter number of sides : 4
+# Enter Length : 5
+# Enter Base : 5
+# Area of Rectangle =  25
+
+
+
 
 
 
