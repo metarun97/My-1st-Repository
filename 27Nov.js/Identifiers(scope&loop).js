4e6e0209124c53e006ad40fbd1172eb3b3088518
TARUN UPADHYAY <116960857+metarun97@users.noreply.github.com>
2022-11-27 16:19:22 +0530

    Add files via upload

---
+++ 27Nov.js/Identifiers(scope&loop).js
@@ -0,0 +1,119 @@
+// Loops and Scopes is Java Script
+
+//    (INPUT)                          (OUTPUT)
+
+console.log("1")                         // 1
+console.log("2")                         // 2
+console.log("3")                         // 3
+console.log("4")                         // 4
+console.log("5")                         // 5
+
+
+// While loop and for loop
+
+// While
+
+// syntax:-  
+// Print the condition 1-5
+
+// while(condition) {
+//    condition is not defined   
+//             }
+
+leti = 0
+while(i<5){
+    console.log(i)                    // 1,2,3,4,5
+    i++
+}
+
+
+
+leti = 0
+while(i<5){
+    console.log(i)                    // 0,1,2,3,4,5 
+    i=i+1
+    }
+
+
+if(5>10){
+    console.log("hello world")        // No output is given
+    }
+
+
+if(5<10){
+    console.log("hello world")         // hello world
+    }
+
+
+if(5>10){
+    console.log("hello world")
+       }
+    else {
+console.log('10 is greater than 5')     // 10 is greater than 5
+    }   
+        
+
+i = 5
+while(i<5){
+    console.log(i)                      // 0 0 0 0 0 0 0.....& code is breaking 
+}
+
+
+i = 5
+while(i>0){
+    console.log(i)
+    i = i-1                             // 5,4,3,2,1
+    }
+
+
+    i = 5
+    while(i !=0)
+    while(i){                           // 5,4,3,2,1
+        console.log(i)
+        i=i-1
+    }
+
+
+    i = 5
+while(i !=0)
+while(i){ console.log(i)                // 5,4,3,2,1
+    i--
+}
+
+
+if(true)
+    console.log("Hello world")          // Hello World
+
+
+// {} curly brackets denotes block of codes.
+
+if(true)
+{
+    console.log('hello world')          // hello world
+console.log('hy there')                 // hi there
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
