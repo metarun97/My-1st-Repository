c13353d69f51aef25a9db101c841f26698d47a54
TARUN UPADHYAY <116960857+metarun97@users.noreply.github.com>
2022-12-01 20:47:21 +0530

    Add files via upload

---
+++ 1Dec.js/function,numbers&math.js
@@ -0,0 +1,79 @@
+//                                                 [Function,Numbers & Math]
+
+// Function in java script:-
+
+// Buildibg blocks in (js) Java scipt
+// Procedure=> a set of statements that perform some task or calculate.
+
+// Programming
+// 1- [(OOPS) Object oriented programming]                    2- Functional programming
+
+
+// Create function:-
+
+// (1) Function declaration
+// (2) Function expressions
+// (3) Anonymous function
+// (4) Arrow function   => (This is the new one)
+// ES-6 (Ecma Script)
+
+// Function Declaration
+
+function name (){}                                            // Sytax
+
+
+// print the number from 1-5
+
+
+// let i = 0
+while(i < 5){
+    console.log(i)                                            // 0 1 2 3 4
+    i++
+}
+
+
+let i = 1
+while(i <= 5){
+    console.log(i)                                            // 1 2 3 4 5
+    i++
+}
+
+
+function printValueFromOnetoFive(){
+    let i = 1
+while(i <= 5){                         
+    console.log(i)                                             // No output
+    i++}
+}
+
+// Now we do calling & invocation of the function.
+
+function printValueFromOnetoFive(){
+    let i = 1
+while(i <= 5){
+    console.log(i)                                            // 1 2 3 4 5
+    i++}
+}
+printValueFromOnetoFive()
+
+// Now output is shown.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
