5de44693e7b276f721c1f3e89cb3633f454baf5d
TARUN UPADHYAY <116960857+metarun97@users.noreply.github.com>
2022-12-07 12:20:42 +0530

    Update varletconst.js

--- 4Dec.js/varletconst.js
+++ 4Dec.js/varletconst.js
@@ -1,2 +1,65 @@
-// Scopes,Lexical Scopes,Invironment,Chain:-
+/ Difference between let,var & const:-
+
+//      (INPUT)                                  (OUTPUT)
+
+function x(){
+    a = 20
+    console.log(a)                              // 20
+}
+x()
+
+
+function x(){
+    a = 20
+}                                              // 20}
+x()
+console.log(a)
+
+
+function x(){
+    var a = 20
+}                                              // a is not defined
+x()
+console.log(a)
+
+
+// From let a = 20 then it is                  //   (20)
+// From const a = 20 then it is                //   (20)
+
+
+// var a = 20
+
+function x(){
+    console.log(a)                             //    (20)                                   
+}
+x() 
+ 
+
+
+function x(){
+    function y(){
+        function z(){
+             a = 20                            //     (20)
+        }
+        z()                              
+    }
+   y()
+}
+x()
+console.log(a)
+
+
+
+function x(){
+        function y(){
+            function z(){
+                var a = 20
+            }
+            z()                                // a is not defined
+        }
+       y()
+    }
+    x()
+    console.log(a)
+
 
