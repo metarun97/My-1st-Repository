6f9097279b3216677d3aa42009d6fab841de8e6b
TARUN UPADHYAY <116960857+metarun97@users.noreply.github.com>
2022-12-15 19:33:14 +0530

    Add files via upload

---
+++ 15Dec.js/object&methods.js
@@ -0,0 +1,32 @@
+//                                                          [OBJECT & METHODS]
+
+// Objects:-
+
+// key : value
+// {} curly brackets are for objescts.
+// Note:- Objects are passed by reference.
+
+// let user = new object   (Object constructor syntax)
+// user
+
+// let user = {}           (Object leteral syntax)
+// user
+
+
+//                                                        [MY PRACTICE]
+
+let user = {
+    name : 'john',
+    age : 30
+}
+user
+
+console.log(user.name)
+console.log(user.name,user.age)
+user.isTeacher =true
+user
+console.log(user['name'])
+
+
+
+
